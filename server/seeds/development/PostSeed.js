d9c083cca77ae1b1b101bd5ff864494ae7a2b10b
Gastón I. Silva E <givanse@gmail.com>
2015-04-17 18:40:17 -0500

    add Post

---
+++ server/seeds/development/PostSeed.js
@@ -0,0 +1,17 @@
+module.exports = [
+  {
+    title: 'Post 1',
+    body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, '+
+          'sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+  },
+  {
+    title: 'Post 2',
+    body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, '+
+          'sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+  },
+  {
+    title: 'Post 3',
+    body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, '+
+          'sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+  }
+];
